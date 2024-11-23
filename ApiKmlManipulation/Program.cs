27fbb619860e3018cbddd4456c778b65784e945d
Tais Prestes <tais.prestes@voxdatati.com.br>
2024-11-23 17:15:04 -0300

    refact: configure Program.cs to resolve KML file path dynamically based on project structure

--- ApiKmlManipulation/Program.cs
+++ ApiKmlManipulation/Program.cs
@@ -1,16 +1,36 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using ApiKMLManipulation.Services;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAnyOrigin",
-        policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+    options.AddPolicy("AllowAnyOrigin", policy =>
+    {
+        policy
+            .AllowAnyOrigin()  
+            .AllowAnyHeader()  
+            .AllowAnyMethod();  
+    });
 });
 
+builder.Services.AddScoped<KmlService>(provider =>
+{
+    var configuration = provider.GetRequiredService<IConfiguration>();
+    var relativePath = configuration["ApplicationSettings:KmlFilePath"];
+
+    var projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+    if (projectDirectory == null)
+    {
+        throw new InvalidOperationException("Não foi possível determinar o diretório raiz do projeto.");
+    }
+
+    var filePath = Path.Combine(projectDirectory, relativePath);
+    return new KmlService(filePath);
+});
+
+builder.Services.AddScoped<PlacemarkService>();
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -26,7 +46,8 @@ app.UseHttpsRedirection();
 
 app.UseCors("AllowAnyOrigin");
 
-app.MapGet("/", () => "ApiKMLManipulation - API est� funcionando!");
+app.MapControllers();
 
+app.MapGet("/", () => "ApiKMLManipulation - API está funcionando!");
 
 app.Run();
